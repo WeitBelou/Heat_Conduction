b71b3ece71adc71b32cb9774c7d2069d4db6985f
SudoBobo <ivan.koptelov@phystech.edu>
2016-04-18 16:38:27 +0300

    add argument struct

--- src/core/argument.h
+++ src/core/argument.h
@@ -1,4 +1,29 @@
 #ifndef ARGUMENT_STRUCT_H
 #define ARGUMENT_STRUCT_H
+#include "./layer.h"
+struct ArgumentForCalc
+{	const Layer zeroLayer;
+	const BoolNet idNet;
+	int tMax;
+	int iMax;
+	int jMax;
+	double tStep;
+	double xStep;
+	double yStep;
+	double p;
+	double lmbd;
+	double c;
+};
+
+struct ArgumentForDraw
+{
+	const QVector<Layer> allLayers;
+	int tMax;
+	int iMax;
+	int jMax;
+	double tStep;
+	double xStep;
+	double yStep;
+};
 
 #endif // ARGUMENT_STRUCT_H
